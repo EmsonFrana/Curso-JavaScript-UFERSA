de02a7d40891b3c5f4c9eaeab2a970ddcf2d4f56
ultraemson <esf12364@gmail.com>
2023-04-12 01:10:44 +0044

    commit

--- atividade/006/index.js
+++ atividade/006/index.js
@@ -1,7 +1,30 @@
-let vetor = [5];
-for (const i in vetor) {
-    if (Object.hasOwnProperty.call(vetor, i)) {
-        const aux = vetor[i];
-        
+
+const vetor = prompt('Informe a lista de números separados por virgulas:')
+
+const lista = []
+for(let i = 0 ; i  < vetor.length; i++){
+    if(vetor[i] !== ","){
+        lista.push(parseInt(vetor[i]))
     }
 }
+
+
+let soma = 0
+const impar = []
+const par = []
+
+for(let j = 0 ; j < lista.length;j++){
+    if(lista[j] > 3 && lista[j] <= 9){
+        soma += lista[j]
+    }
+
+    if(lista[j] % 2 === 0){
+        par.push(lista[j])
+        console.log("par "+par[j]);
+    }
+    else{
+        impar.push(lista[j])
+        console.log("Impar : "+impar[j]);
+    }
+}
+console.log(soma);
