da9a53072a3f85a561d6250427f2b839003c4491
ultraemson <esf12364@gmail.com>
2023-04-25 20:23:07 -0300

    aula08

---
+++ src/aula08/index.js
@@ -0,0 +1,35 @@
+// function mostra(name){
+//     return `Olá, ${name}`
+// }
+
+
+// const name = mostra('emson')
+
+// document.write(name)
+
+// function soma(a,b){
+//     return a+b
+// }
+// const x = parseInt(prompt("Didite um número: "))
+// const y = parseInt(prompt("Didite um número: "))
+// const resultado = soma(x,y)
+
+// document.write(`A soma de ${x} + ${y} = ${resultado}`)
+
+//na função abaixo só será executada quando chegar nela(função anonima)
+// const soma = function(a,b) {
+//     document.write(a + b)
+// }
+
+// soma(10,45)
+//acessando o tag button no html
+// let botao = document.querySelector('#id')
+
+// botao.addEventListener('click', function(){
+//     alert("la ele")
+// })
+//arrow function
+const soma = (a,b)=>{
+    return a +b
+
+document.write(soma)}
